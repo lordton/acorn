1fce3a66785f21455bde6a18aa421e68199d9474
lordton <lord-ton@mail.ru>
2015-09-24 01:36:47 +0300

    time to sleep

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
 $( document ).ready(function() {
     $("#gyro2").text( "The DOM is now loaded and can be manipulated." );
     console.log("yesp")
+
   });
+
+
+
+
+new MediaElement('player1', {
+    // shows debug errors on screen
+    enablePluginDebug: false,
+    // remove or reorder to change plugin priority
+    plugins: ['flash','silverlight'],
+    // specify to force MediaElement to use a particular video or audio type
+    type: '',
+    // path to Flash and Silverlight plugins
+    pluginPath: '/myjsfiles/',
+    // name of flash file
+    flashName: 'flashmediaelement.swf',
+    // name of silverlight file
+    silverlightName: 'silverlightmediaelement.xap',
+    // default if the <video width> is not specified
+    defaultVideoWidth: 1000,
+    // default if the <video height> is not specified     
+    defaultVideoHeight: 270,
+    // overrides <video width>
+    pluginWidth: -1,
+    // overrides <video height>       
+    pluginHeight: -1,
+    // rate in milliseconds for Flash and Silverlight to fire the timeupdate event
+    // larger number is less accurate, but less strain on plugin->JavaScript bridge
+    timerRate: 250,
+    // method that fires when the Flash or Silverlight object is ready
+    success: function (mediaElement, domObject) { 
+         
+        // add event listener
+        mediaElement.addEventListener('timeupdate', function(e) {
+             
+            document.getElementById('current-time').innerHTML = mediaElement.currentTime;
+             
+        }, false);
+         
+        // call the play method
+        mediaElement.play();
+         
+    },
+    // fires when a problem is detected
+    error: function () { 
+     
+    }
+});
+
 
 
 window.ondeviceorientation = function(event) {
@@ -51,8 +100,6 @@ $("#gyro6 ").text("az "+Math.round(az*10));
 
   }
 }
-
-
 
 
 
