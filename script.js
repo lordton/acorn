c20670425163f7b46e963db249b2b57e3f7463a0
lordton <lord-ton@mail.ru>
2015-09-25 12:19:52 +0300

    not working

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ $( document ).ready(function() {
     $("#gyro2").text( "JS ok." );
     console.log("yesp")
 
-
-
   $('video').mediaelementplayer({
     alwaysShowControls: true,
     videoVolume: 'horizontal',
     features: ['playpause','progress','volume','fullscreen']
+
   });
 
-
+var level = 0;
+console.log("level111111111111111111 "+level)
 
 $( "#button1" ).click(function() {     
     $('#modal1').hide();
     console.log("click");
     $('video')[0].player.play();
+    level = 1;
     });
 
 
@@ -29,30 +30,45 @@ window.ondeviceorientation = function(event) {
   beta = Math.round(event.beta*10);
   gamma = Math.round(event.gamma*10);
 $("#gyro1").text("alpha "+alpha);
+console.log(level);
+console.log(window.level);
+console.log(document.level);
+
+
 
 //почасовой против
-if (beta > 100 ){
+if (beta > 100 && event.level == 1){
   $("#gyro2").text("beta "+"против часовой "+beta);
   $('video')[0].player.pause();
+  $('#modal1').show();
+  $("#text1").text( "Верните телефон в правильное положение (Против часовой стрелки)" );
 }
-if (beta < -100) {
+if (beta < -100 && event.level == 1) {
   $("#gyro2").text("beta "+"по часовой "+beta);
   $('video')[0].player.pause();
+  $('#modal1').show();
+  $("#text1").text( "Верните телефон в правильное положение (По часовой стрелке)" );
 }
-if (beta >= -100 && beta <=100 && gamma >= -700 && gamma <= -500 ) {
+if (beta >= -100 && beta <=100 && gamma >= -700 && gamma <= -500 && event.level == 1 ) {
   $("#gyro2").text("beta "+"OK "+beta);
   $("#gyro3").text("gamma "+"ОК "+gamma);
   $('video')[0].player.play();
+  $('#modal1').hide();
+
 }
 
 //наклон вперед назад
-if (gamma > -500) {
+if (gamma > -500 && event.level == 1) {
   $("#gyro3").text("gamma "+"на себя "+gamma);
   $('video')[0].player.pause();
+  $('#modal1').show();
+  $("#text1").text( "Верните телефон в правильное положение (На себя)" );
 }
-if (gamma < -700) {
+if (gamma < -700 && event.level == 1) {
   $("#gyro3").text("gamma "+"от себя "+gamma);
   $('video')[0].player.pause();
+  $('#modal1').show();
+  $("#text1").text( "Верните телефон в правильное положение (От себя)" );
 }
 
 
@@ -75,8 +91,6 @@ $("#gyro6 ").text("az "+Math.round(az*10));
   }
 }
 
-Interval 
 
-Consol.log
 
 
