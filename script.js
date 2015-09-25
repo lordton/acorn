7a185ee74dcfa6a84875b61cd3a95a14b627f81b
lordton <lord-ton@mail.ru>
2015-09-25 15:44:19 +0300

    works!

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ $( document ).ready(function() {
   });
 
 var level = 0;
+var restcounter = 0;
+var restcountertext = "счетчик";
 console.log("level111111111111111111 "+level);
 
 $( "#button1" ).click(function() {     
@@ -17,11 +19,6 @@ $( "#button1" ).click(function() {
     $('video')[0].player.play();
     level = 1;
     });
-
-
-
-
-
 
 
 window.ondeviceorientation = function(event) {
@@ -73,10 +70,55 @@ window.ondevicemotion = function(event) {
   ax = event.accelerationIncludingGravity.x
   ay = event.accelerationIncludingGravity.y
   az = event.accelerationIncludingGravity.z
+  bx = event.acceleration.x
+  by = event.acceleration.y
+  bz = event.acceleration.z
   rotation = event.rotationRate;
+  rest = (Math.abs(Math.round(bx*1000))+Math.abs(Math.round(by*1000))+Math.abs(Math.round(bz*1000)))/3
 $("#gyro4").text("ax "+Math.round(ax*10));
 $("#gyro5").text("ay "+Math.round(ay*10));
-$("#gyro6 ").text("az "+Math.round(az*10));
+$("#gyro6").text("az "+Math.round(az*10));
+
+//console.log("ax "+Math.round(ax*1000)+";ay "+Math.round(ay*1000)+";az "+Math.round(az*1000)+";bx "+Math.round(bx*1000)+";by "+Math.round(by*1000)+";bz "+Math.round(bz*1000))
+console.log(rest);
+$("#gyro7").text(restcountertext);
+
+
+
+
+if (rest <= 70){
+  restcounter = restcounter +1; 
+};
+if (rest > 70 && rest <= 150){
+  restcounter = restcounter -1; 
+};
+if (rest > 150 && rest <= 400){
+  restcounter = restcounter -3; 
+};
+if (rest > 400 && rest <= 1000){
+  restcounter = restcounter + 10; 
+};
+
+if (rest > 1000 && rest <= 2000){
+  restcounter = restcounter + 100; 
+};
+
+
+if (restcounter<=0){
+  restcounter=0
+};
+
+if (restcounter>0 && restcounter<300){
+  restcountertext=restcounter
+};
+
+if (restcounter>=300){
+  restcountertext="плохо"
+};
+
+if (restcounter>=400){
+  restcounter=0
+};
 
   if (rotation != null) {
     arAlpha = Math.round(rotation.alpha);
